a6124bfc925471e43347edde650e89db43bbdbed
shikharmanandhar <shikharaa26@gmail.com>
2022-01-28 08:47:39 +0545

    edited contact page

--- src/components/layout/Contact/Contact.js
+++ src/components/layout/Contact/Contact.js
@@ -4,7 +4,9 @@ import "./Contact.css";
 const Contact = () => {
   return <Fragment>
       <div className="contactContainer">
+          {/* <img src='http://cdn.onlinewebfonts.com/svg/img_52216.png'></img> */}
           <a className="mailBtn" href="mailto:shikharaa26@gmail.com">
+              <img src='http://cdn.onlinewebfonts.com/svg/img_52216.png'></img>
               <button>Contact: shikharaa26@gmail.com</button>
           </a>
       </div>
