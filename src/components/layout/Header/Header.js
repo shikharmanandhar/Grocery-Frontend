3d40f11d06bc215abbb7ac645064ac1bfcd08f8f
shikharmanandhar <shikharaa26@gmail.com>
2022-02-05 09:31:41 +0545

    edited header .js

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
         <div className="container-fluid">
          
           <Link className="navbar-brand" to="/">FoodCourt</Link>
+          <img src='../../../images/logo.png'></img>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
