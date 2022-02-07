fd64042556bf51fdb213667af846953b8edc9374
shikharmanandhar <shikharaa26@gmail.com>
2022-02-07 09:03:39 +0545

    cart item edited

--- src/components/Cart/CartItemCard.js
+++ src/components/Cart/CartItemCard.js
@@ -9,7 +9,7 @@ const CartItemCard = ({item,deleteCartItems}) => {
         
         <div>
         <Link to={`/product/${item.product}`}>{item.name}</Link>
-        <span>{`Price: ₹${item.price}`}</span>
+        <span>{`Price: Nrs.${item.price}`}</span>
         <p onClick={() => deleteCartItems(item.product)}>Remove</p>
         </div>
         </div>
