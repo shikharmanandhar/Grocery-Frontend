56c5b584121eb37b082c8093f50205355854f3a7
shikharmanandhar <shikharaa26@gmail.com>
2022-02-12 19:08:37 +0545

    removed credit card icon

--- src/components/Cart/Payment.js
+++ src/components/Cart/Payment.js
@@ -7,7 +7,7 @@ import { useAlert } from "react-alert";
 import { CardNumberElement, CardCvcElement, CardExpiryElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import axios from "axios";
 import "./Payment.css"
-import CreditCardIcon from "@material-ui/icons/CreditCard";
+// import CreditCardIcon from "@material-ui/icons/CreditCard";
 // import EventIcon from "@material-ui/icons/Event";
 // import VpnKeyIcon from "@material-ui/icons/VpnKey";
 import { useNavigate } from 'react-router-dom';
