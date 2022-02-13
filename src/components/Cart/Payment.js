f71f00ccc39a85a3ad809dcea24b0e9b0e581823
shikharmanandhar <shikharaa26@gmail.com>
2022-02-13 19:09:06 +0545

    removed elements of payments

--- src/components/Cart/Payment.js
+++ src/components/Cart/Payment.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import MetaData from "../layout/MetaData"
 import { Typography } from "@material-ui/core";
 import { useAlert } from "react-alert";
-import { CardNumberElement, CardCvcElement, CardExpiryElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import axios from "axios";
+// import { CardNumberElement, CardCvcElement, CardExpiryElement, useStripe, useElements } from "@stripe/react-stripe-js";
+// import axios from "axios";
 import "./Payment.css"
 // import CreditCardIcon from "@material-ui/icons/CreditCard";
 // import EventIcon from "@material-ui/icons/Event";
